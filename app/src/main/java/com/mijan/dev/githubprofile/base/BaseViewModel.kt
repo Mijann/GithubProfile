50920aacd58df4f0a136eefa914995a07ac0338d
Mohammad Hamizan <hamizan.lamit@setel.my>
2022-11-05 20:41:30 +0800

    #4 - Add generic function callApi for BaseViewModel to handle api response for different states

--- app/src/main/java/com/mijan/dev/githubprofile/base/BaseViewModel.kt
+++ app/src/main/java/com/mijan/dev/githubprofile/base/BaseViewModel.kt
@@ -3,6 +3,8 @@ package com.mijan.dev.githubprofile.base
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mijan.dev.githubprofile.data.model.AppError
+import com.mijan.dev.githubprofile.data.model.Resource
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
@@ -23,6 +25,32 @@ open class BaseViewModel : ViewModel() {
     fun emitLoading(showLoading: Boolean) {
         viewModelScope.launch {
             _loadingFlow.emit(showLoading)
+        }
+    }
+}
+
+fun <T> BaseViewModel.callApi(
+    flow: Flow<Resource<T>>,
+    onSuccess: (data: T) -> Unit,
+    onFailure: ((error: AppError) -> Unit)? = null,
+    showLoading: Boolean = true,
+) {
+    viewModelScope.launch {
+        emitLoading(showLoading)
+        flow.collect {
+            emitLoading(false)
+            when (it) {
+                is Resource.Success -> {
+                    onSuccess.invoke(it.data)
+                }
+                is Resource.Failure -> {
+                    if (onFailure == null) {
+                        emitError(it.appError)
+                    } else {
+                        onFailure.invoke(it.appError)
+                    }
+                }
+            }
         }
     }
 }
