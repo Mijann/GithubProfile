058e9155c2e4ce336f0921ff031b69f79180c324
Mohammad Hamizan <hamizan.lamit@setel.my>
2022-11-06 10:02:31 +0800

    #9 - Add UserEntity - Create UserDao for adding new users and get users from database

---
+++ app/src/main/java/com/mijan/dev/githubprofile/data/local/dao/UserDao.kt
@@ -0,0 +1,18 @@
+package com.mijan.dev.githubprofile.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.mijan.dev.githubprofile.data.local.entity.UserEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addUsers(entities: List<UserEntity>): List<Long>
+
+    @Query("SELECT * FROM users")
+    fun getUsers(): Flow<List<UserEntity>>
+
+}
