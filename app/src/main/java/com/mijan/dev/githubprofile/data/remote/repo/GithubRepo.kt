8e1bd0b49d340f62e53d70bbbc3ff84ecd8577de
Mohammad Hamizan <hamizan.lamit@setel.my>
2022-11-06 19:33:45 +0800

    #11 - Add getUsersPagingDataFlow for users paging data flow

--- app/src/main/java/com/mijan/dev/githubprofile/data/remote/repo/GithubRepo.kt
+++ app/src/main/java/com/mijan/dev/githubprofile/data/remote/repo/GithubRepo.kt
@@ -1,6 +1,13 @@
 package com.mijan.dev.githubprofile.data.remote.repo
 
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import com.mijan.dev.githubprofile.data.UsersRemoteMediator
+import com.mijan.dev.githubprofile.data.local.AppDatabase
+import com.mijan.dev.githubprofile.data.local.dao.UserDao
 import com.mijan.dev.githubprofile.data.model.Resource
+import com.mijan.dev.githubprofile.data.model.toUserEntity
 import com.mijan.dev.githubprofile.data.remote.api.GithubApi
 import com.mijan.dev.githubprofile.utils.toAppError
 import kotlinx.coroutines.flow.flow
@@ -9,8 +16,18 @@ import javax.inject.Singleton
 
 @Singleton
 class GithubRepo @Inject constructor(
-    private val githubApi: GithubApi
+    private val githubApi: GithubApi,
+    private val database: AppDatabase,
+    private val userDao: UserDao,
 ) {
+    @ExperimentalPagingApi
+    fun getUsersPagingDataFlow(pageSize: Int) = Pager(
+        config = PagingConfig(pageSize),
+        remoteMediator = UsersRemoteMediator(database = database, userDao, githubApi)
+    ) {
+        userDao.getUsersPagingSource()
+    }.flow
+
     fun getUsers() = flow {
         try {
             emit(Resource.Success(githubApi.getUsers()))
