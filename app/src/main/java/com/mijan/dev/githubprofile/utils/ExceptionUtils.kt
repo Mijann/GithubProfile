79f8176b1227d457fbf23ed17fc8920db8ee1b60
Mohammad Hamizan <hamizan.lamit@setel.my>
2022-11-11 11:49:03 +0800

    #15 - Update ExceptionUtils to cater for updated AppError data class

--- app/src/main/java/com/mijan/dev/githubprofile/utils/ExceptionUtils.kt
+++ app/src/main/java/com/mijan/dev/githubprofile/utils/ExceptionUtils.kt
@@ -1,5 +1,14 @@
 package com.mijan.dev.githubprofile.utils
 
+import com.mijan.dev.githubprofile.R
 import com.mijan.dev.githubprofile.data.model.AppError
+import java.net.ConnectException
+import java.net.UnknownHostException
 
-fun Exception.toAppError() = AppError(errorMessage = message.orEmpty())
+fun Exception.toAppError(): AppError {
+    return when (this) {
+        is UnknownHostException -> AppError(errorResId = R.string.network_error_no_connection)
+        is ConnectException -> AppError(errorResId = R.string.network_error_no_connection)
+        else -> AppError(errorMessage = message.orEmpty())
+    }
+}
