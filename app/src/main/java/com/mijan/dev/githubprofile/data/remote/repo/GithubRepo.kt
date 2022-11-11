470570a473cbb0eacf88a455dcbd1b8f2557be4b
Mohammad Hamizan <hamizan.lamit@setel.my>
2022-11-11 11:47:07 +0800

    #15 - Update GithubRepo to cater for get single user by username from api and cache suer (local db user)

--- app/src/main/java/com/mijan/dev/githubprofile/data/remote/repo/GithubRepo.kt
+++ app/src/main/java/com/mijan/dev/githubprofile/data/remote/repo/GithubRepo.kt
@@ -3,13 +3,16 @@ package com.mijan.dev.githubprofile.data.remote.repo
 import androidx.paging.ExperimentalPagingApi
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
+import androidx.room.withTransaction
 import com.mijan.dev.githubprofile.data.UsersRemoteMediator
 import com.mijan.dev.githubprofile.data.local.AppDatabase
 import com.mijan.dev.githubprofile.data.local.dao.UserDao
+import com.mijan.dev.githubprofile.data.model.GithubUser
 import com.mijan.dev.githubprofile.data.model.Resource
 import com.mijan.dev.githubprofile.data.model.toUserEntity
 import com.mijan.dev.githubprofile.data.remote.api.GithubApi
 import com.mijan.dev.githubprofile.utils.toAppError
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -28,8 +31,14 @@ class GithubRepo @Inject constructor(
         userDao.getUsersPagingSource()
     }.flow
 
+    suspend fun getCacheUsers() = userDao.getUsers()
+
     fun getUsers() = flow {
         try {
+            val users = githubApi.getUsers()
+            database.withTransaction {
+                userDao.addOrUpdateUsers(users.map(GithubUser::toUserEntity))
+            }
             emit(Resource.Success(githubApi.getUsers()))
         } catch (e: Exception) {
             emit(Resource.Failure(e.toAppError()))
@@ -38,7 +47,24 @@ class GithubRepo @Inject constructor(
 
     fun getUser(username: String) = flow {
         try {
-            emit(Resource.Success(githubApi.getUser(username)))
+            val user = githubApi.getUser(username)
+            database.withTransaction {
+                userDao.addOrUpdateUser(user.toUserEntity())
+            }
+            emit(Resource.Success(user))
+        } catch (e: Exception) {
+            emit(Resource.Failure(e.toAppError()))
+        }
+    }
+
+    fun getCacheUser(username: String) = userDao.getUserFlow(username).distinctUntilChanged()
+
+    fun updateUserNotes(username: String, notes: String) = flow {
+        try {
+            database.withTransaction {
+                userDao.updateUserNotes(username, notes)
+            }
+            emit(Resource.Success("Notes saved success"))
         } catch (e: Exception) {
             emit(Resource.Failure(e.toAppError()))
         }
